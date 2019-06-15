a5cb02982bf012f3717d66c557bf34b6ed3d4784
Erickson Tavares <erickson@erickson-lenovo.meuintelbras.local>
2019-06-15 00:39:18 -0300

    importando estilos da pagina de login

--- login.php
+++ login.php
@@ -1,119 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<!-- Meta, title, CSS, favicons, etc. -->
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gentelella Alela! | </title>
 
-	<title> SGC - Autenticação </title>
+    <!-- Bootstrap -->
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="vendors/animate.css/animate.min.css" rel="stylesheet">
 
-	<link rel="icon" href="/images/favicon.ico" type="image/x-icon"/>
+    <!-- Custom Theme Style -->
+    <link href="build/css/custom.min.css" rel="stylesheet">
+  </head>
 
-	<link href="/css/bootstrap_4.3/bootstrap.min.css" rel="stylesheet">
-
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/v4-shims.css">
-	<link href="/css/animate.min.css" rel="stylesheet">
-
-	<link href="/css/pnotify/pnotify.css" rel="stylesheet">
-  <link href="/css/pnotify/pnotify.buttons.css" rel="stylesheet">
-  <link href="/css/pnotify/pnotify.nonblock.css" rel="stylesheet">
-	
-	<link href="/css/custom.css" rel="stylesheet">
-	<link href="/css/complement.css" rel="stylesheet">
-	
-	<script src="/js/jquery-3.4.0.min.js"></script>
-	<script src="/js/bootstrap_4.3/bootstrap.min.js"></script>
-
-	<script src="/js/pnotify/pnotify.js"></script>
-	<script src="/js/pnotify/pnotify.buttons.js"></script>
-	<script src="/js/pnotify/pnotify.nonblock.js"></script>
-
-	<script src="/js/utils.js"></script>
-	<script src="/js/user.js"></script>
-	
-</head>
-
-<body class="login">
+  <body class="login">
     <div>
-	
-		<a class="hiddenanchor" id="lostpass"></a>
-		<a class="hiddenanchor" id="signin"></a>
-	
-		<div class="login_wrapper">
-		
-			<div class="animate form login_form">
-				<section class="login_content">
-					<form onSubmit="objUser.login(); return false;">
-						<h1> Autenticação </h1>
-						<div>
-							<input class="form-control" type="text" id="user" name="user" placeholder="Usuário" />
-						</div>
-						<div>
-							<input class="form-control" type="password" id="pass" name="pass" placeholder="Senha" />
-						</div>
-						<div>
-							
-							<input class="btn btn-outline-secondary btn-sm" type="submit" value="Entrar" />
-							<a class="reset_pass" href="#lostpass"> Perdeu sua senha? </a>
-						</div>
-						
-						<div class="clearfix"></div>
-						
-						<div class="separator">
-							<img src="image.php?src=images/logo.png&w=146&h=158">
-						</div>
-					</form>
-					
-				</section>
-				
-				<div class="loading_login_form loading"> </div>
-			</div>
-			
-			<div id="formlostpass" class="animate form lostpass_form">
-    		
-    			<section class="login_content">
-    			
-    				<form onSubmit='objUser.recoverPassword(); return false;'>
-    					<h1> Recuperar Senha </h1>
-							
-							<div class="alert" style="display:none;"></div>
-    					
-    					<div>
-    						<input type="text" class="form-control" placeholder="Informe seu E-mail" id="email" name="email" />
-    					</div>
-    					<div>
-    						<input type="submit" class="btn btn-outline-secondary btn-sm" value="Enviar" />
-    					</div>
-    					
-    					<div class="clearfix"></div>
-    					
-    					<div class="separator">
-    						<p class="change_link">Lembrou seus dados?
-    							<a href="#signin" class="to_register"> Entrar </a>
-    						</p>
-    					
-    						<div class="clearfix"></div>
-    						<br />
-    					
-    						<div>
-    							<h1> <img src="/images/logo.png" /> </h1>
-    						</div>
-    					</div>
-    				</form>
-    			</section>
-    			
-    			<div class="l-lostpass_form loading"> </div>
-    			
-    		</div>
-			
-		</div>
-	
-	</div>
-	
-</body>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
 
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <form>
+              <h1>Login Form</h1>
+              <div>
+                <input type="text" class="form-control" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" placeholder="Password" required="" />
+              </div>
+              <div>
+                <a class="btn btn-default submit" href="index.html">Log in</a>
+                <a href="#signup" class="to_register">Lost your password?</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+            <form>
+              <h1>Request Password</h1>
+              
+              <div>
+                <input type="email" class="form-control" placeholder="Email" required="" />
+              </div>
+              
+              <div>
+                <a class="btn btn-default submit" href="index.html">Submit</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">you remember?
+                  <a href="#signin" class="to_register"> Log in </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h1><i class="fa fa-paw"></i> Gentelella Alela!</h1>
+                  <p>©2016 All Rights Reserved. Gentelella Alela! is a Bootstrap 3 template. Privacy and Terms</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
 </html>
